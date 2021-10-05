5635bacf9d0bdef5e67dc03a524fe28c28fe636a
stamdvarim1000 <stamdvarim1000@gmail.com>
2021-10-05 17:25:24 +0300

    style

--- src/Components/FormInput.jsx
+++ src/Components/FormInput.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import LinkButton from './Button';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 import '../App.css'
 
@@ -22,38 +27,61 @@ const ValidtionBalss = (sumBalls) => {
 export default function FormInput(props) {
     return (
         <div className="GameForm">
-            <div className="form">
-                <div>
-                    <h3>כמות המשתתפים צריכה להיות בין 2 ל-5 </h3>
-                    <h3>כמות הכדורים צריכה להיות בין 1 ל 3 </h3>
+            <div className='form'>
+                <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">כמות שחקנים</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={props.players}
+                            label="כמות שחקנים"
+                            onChange={
+                                e => {
+                                    if (Validtion(e.target.value)) {
+                                        props.setPlayers(e.target.value);
+                                    } else {
+                                        e.target.value = props.players;
+                                        alert("מספר המשתתפים צריך להיות גדול מ2 וקטן מ5");
+                                    }
+                                }
+                            }
+                        >
+                            <MenuItem value={2}>2</MenuItem>
+                            <MenuItem value={3}>3</MenuItem>
+                            <MenuItem value={4}>4</MenuItem>
+                            <MenuItem value={5}>5</MenuItem>
 
-                </div>
-                <div>
-                    <label > כמות משתתפים - </label>
-                    <input type="number" placeholder={props.players} value={props.players} onFocus={(e) => { e.target.value = "" }} onChange={
-                        e => {
-                            if (Validtion(e.target.value)) {
-                                props.setPlayers(e.target.value);
-                            } else {
-                                e.target.value = props.players;
-                                alert("מספר המשתתפים צריך להיות גדול מ2 וקטן מ5");
+                        </Select>
+                    </FormControl>
+                </Box>
+
+                <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">כמות כדורים לשחקן</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={props.sumBalls}
+                            label="כמות כדורים לשחקן"
+                            onChange={
+                                e => {
+                                    if (ValidtionBalss(e.target.value)) {
+                                        props.setBalls(e.target.value);
+                                    } else {
+                                        e.target.value = props.sumBalls;
+                                        alert("כמות הכדורים חייבת להיות בין 1 ל 3 ");
+                                    }
+                                }
                             }
-                        }
-                    } />
-                </div>
-                <div>
-                    <label > כמות כדורים לשחקן - </label>
-                    <input type="number" placeholder={props.sumBalls} value={props.sumBalls} onFocus={(e) => { e.target.value = "" }} onChange={
-                        e => {
-                            if (ValidtionBalss(e.target.value)) {
-                                props.setBalls(e.target.value);
-                            } else {
-                                e.target.value = props.sumBalls;
-                                alert("כמות הכדורים חייבת להיות בין 1 ל 3 ");
-                            }
-                        }
-                    } />
-                </div>
+                        >
+                            <MenuItem value={1}>1</MenuItem>
+                            <MenuItem value={2}>2</MenuItem>
+                            <MenuItem value={3}>3</MenuItem>
+
+                        </Select>
+                    </FormControl>
+                </Box>
                 <div className="center">
                     <LinkButton enabled={Validtion(props.players)} route="/RandomNumber" text="הגרל!"></LinkButton>
                 </div>
